f8e976b0aed882485ffe727b10742d210dde21a7
Enes Algan <ensalgn76@gmail.com>
2023-07-04 10:36:46 +0300

    register activity created

---
+++ app/src/main/java/com/example/kotlininstagramapp/Login/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlininstagramapp.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlininstagramapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
